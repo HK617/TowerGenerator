891175f8b17c8004e2cd6f0961c59393cdeaeb3d
HK617 <158565108+HK617@users.noreply.github.com>
2025-11-23 20:17:44 +0900

    ドローンの詳細メニュー欄を追加（中身はまだない）

--- DroneListItemUI.cs
+++ DroneListItemUI.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class DroneListItemUI : MonoBehaviour
+public class DroneListItemUI : MonoBehaviour, IPointerClickHandler
 {
     public Image background;
     public TMP_Text titleText;
     public TMP_Text subText;
     public Slider progress;
+
+    // ★ このスロットに紐づく Drone と、クリック時に呼ばれるコールバック
+    [System.NonSerialized] public DroneWorker boundDrone;
+    [System.NonSerialized] public System.Action<DroneListItemUI> onClick;
 
     public void SetTitle(string t)
     {
@@ -27,5 +32,11 @@ public class DroneListItemUI : MonoBehaviour
     public void SetProgress(float v)
     {
         if (progress) progress.value = Mathf.Clamp01(v);
+    }
+
+    // ★ クリックされたときに UI システムから呼ばれる
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClick?.Invoke(this);
     }
 }
